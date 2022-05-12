55eb0de5705f113234d40976a3c47c263773476c
JUNO <jrlee0701@kakao.com>
2022-05-12 21:00:23 +0900

    Initial Commit

---
+++ NotiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotiApp
+//
+//  Created by JUNO on 2022/05/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
